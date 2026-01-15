c1205a5b54c4e0a836a7b4a0886d2da321e7c02d
666RED <hongchensee8@gmail.com>
2026-01-16 07:42:09 +0800

    Update github repo url on sidebar

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -47,7 +47,7 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: "Github Repo",
-        href: "https://github.com/laravel/vue-starter-kit",
+        href: "https://github.com/666RED/Morph-digital-assessment.git",
         icon: Folder,
     },
 ];
